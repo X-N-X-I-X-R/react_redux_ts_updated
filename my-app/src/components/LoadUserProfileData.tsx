281c33d5f9e02fad1999a4c7770d4b315d4f4540
01001110IR <nirstam@gmail.com>
2024-06-28 03:35:02 +0300

    fixing load data

--- my-app/src/components/LoadUserProfileData.tsx
+++ my-app/src/components/LoadUserProfileData.tsx
@@ -2,16 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from '../store/hooks';
 import { fetchUserProfile, updateUserProfile, UserProfileInterface } from '../store/slicers/userProfileSlice';
 import { RootState } from '../store/rootReducer';
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, TextField, MenuItem, Typography, Select, InputLabel, FormControl, Snackbar } from '@mui/material';
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, TextField, MenuItem, Typography, Select, InputLabel, FormControl, Snackbar, SelectChangeEvent } from '@mui/material';
 import moment from 'moment-timezone';
 import styled from '@emotion/styled';
-import { countries } from 'countries-list'; // קבלת רשימת המדינות
+import { countries } from 'countries-list';
 
-const StyledTableCellFirstColumn = styled(TableCell)`
-  border-right: 2px solid #9c27b0;
-`;
-
-const StyledTableCellSecondColumn = styled(TableCell)``;
+type TCountryCode = 'AD' | 'AE' | 'AF' | 'AG' | 'AI' | 'AL' | 'AM' | 'AO' | 'AQ' | 'AR' | 'AS' | 'AT' | 'AU' | 'AW' | 'AX' | 'AZ' | 'BA' | 'BB' | 'BD' | 'BE' | 'BF' | 'BG' | 'BH' | 'BI' | 'BJ' | 'BL' | 'BM' | 'BN' | 'BO' | 'BQ' | 'BR' | 'BS' | 'BT' | 'BV' | 'BW' | 'BY' | 'BZ' | 'CA' | 'CC' | 'CD' | 'CF' | 'CG' | 'CH' | 'CI' | 'CK' | 'CL' | 'CM' | 'CN' | 'CO' | 'CR' | 'CU' | 'CV' | 'CW' | 'CX' | 'CY' | 'CZ' | 'DE' | 'DJ' | 'DK' | 'DM' | 'DO' | 'DZ' | 'EC' | 'EE' | 'EG' | 'EH' | 'ER' | 'ES' | 'ET' | 'FI' | 'FJ' | 'FK' | 'FM' | 'FO' | 'FR' | 'GA' | 'GB' | 'GD' | 'GE' | 'GF' | 'GG' | 'GH' | 'GI' | 'GL' | 'GM' | 'GN' | 'GP' | 'GQ' | 'GR' | 'GS' | 'GT' | 'GU' | 'GW' | 'GY' | 'HK' | 'HM' | 'HN' | 'HR' | 'HT' | 'HU' | 'ID' | 'IE' | 'IL' | 'IM' | 'IN' | 'IO' | 'IQ' | 'IR' | 'IS' | 'IT' | 'JE' | 'JM' | 'JO' | 'JP' | 'KE' | 'KG' | 'KH' | 'KI' | 'KM' | 'KN' | 'KP' | 'KR' | 'KW' | 'KY' | 'KZ' | 'LA' | 'LB' | 'LC' | 'LI' | 'LK' | 'LR' | 'LS' | 'LT' | 'LU' | 'LV' | 'LY' | 'MA' | 'MC' | 'MD' | 'ME' | 'MF' | 'MG' | 'MH' | 'MK' | 'ML' | 'MM' | 'MN' | 'MO' | 'MP' | 'MQ' | 'MR' | 'MS' | 'MT' | 'MU' | 'MV' | 'MW' | 'MX' | 'MY' | 'MZ' | 'NA' | 'NC' | 'NE' | 'NF' | 'NG' | 'NI' | 'NL' | 'NO' | 'NP' | 'NR' | 'NU' | 'NZ' | 'OM' | 'PA' | 'PE' | 'PF' | 'PG' | 'PH' | 'PK' | 'PL' | 'PM' | 'PN' | 'PR' | 'PS' | 'PT' | 'PW' | 'PY' | 'QA' | 'RE' | 'RO' | 'RS' | 'RU' | 'RW' | 'SA' | 'SB' | 'SC' | 'SD' | 'SE' | 'SG' | 'SH' | 'SI' | 'SJ' | 'SK' | 'SL' | 'SM' | 'SN' | 'SO' | 'SR' | 'SS' | 'ST' | 'SV' | 'SX' | 'SY' | 'SZ' | 'TC' | 'TD' | 'TF' | 'TG' | 'TH' | 'TJ' | 'TK' | 'TL' | 'TM' | 'TN' | 'TO' | 'TR' | 'TT' | 'TV' | 'TW' | 'TZ' | 'UA' | 'UG' | 'UM' | 'US' | 'UY' | 'UZ' | 'VA' | 'VC' | 'VE' | 'VG' | 'VI' | 'VN' | 'VU' | 'WF' | 'WS' | 'XK' | 'YE' | 'YT' | 'ZA' | 'ZM' | 'ZW';
 
 const StyledTableCell = styled(TableCell)({
   fontSize: '1rem',
@@ -77,14 +73,28 @@ const LoadUserProfileData: React.FC = () => {
     setEditMode(!editMode);
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | { target: { name?: string; value: unknown } }) => {
+  const handleTextFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setEditedProfile((prev) => {
       if (!prev) return undefined;
 
       const updatedProfile: UserProfileInterface = {
         ...prev,
-        [name as string]: value as string,
+        [name]: value,
+      };
+
+      return updatedProfile;
+    });
+  };
+
+  const handleSelectChange = (e: SelectChangeEvent<TCountryCode>) => {
+    const { name, value } = e.target;
+    setEditedProfile((prev) => {
+      if (!prev) return undefined;
+
+      const updatedProfile: UserProfileInterface = {
+        ...prev,
+        [name]: value as TCountryCode,
       };
 
       return updatedProfile;
@@ -136,8 +146,8 @@ const LoadUserProfileData: React.FC = () => {
                         <InputLabel>{key.replace(/user_/g, '').replace(/_/g, ' ').toUpperCase()}</InputLabel>
                         <Select
                           name={key}
-                          value={editedProfile ? editedProfile[key] : ''}
-                          onChange={handleChange}
+                          value={editedProfile ? editedProfile[key] : value || ' ' }
+                          onChange={handleSelectChange}
                         >
                           <MenuItem value="M">Male</MenuItem>
                           <MenuItem value="F">Female</MenuItem>
@@ -151,12 +161,12 @@ const LoadUserProfileData: React.FC = () => {
                         <InputLabel>{key.replace(/user_/g, '').replace(/_/g, ' ').toUpperCase()}</InputLabel>
                         <Select
                           name={key}
-                          value={editedProfile ? editedProfile[key] : ''}
-                          onChange={handleChange}
+                          value={editedProfile ? (editedProfile[key] as TCountryCode) : ''}
+                          onChange={handleSelectChange}
                         >
                           {Object.keys(countries).map((countryCode) => (
-                            <MenuItem key={countryCode} value={countryCode}>
-                              {countries[countryCode].name}
+                            <MenuItem key={countryCode} value={countryCode as TCountryCode}>
+                              {countries[countryCode as TCountryCode].name}
                             </MenuItem>
                           ))}
                         </Select>
@@ -169,7 +179,7 @@ const LoadUserProfileData: React.FC = () => {
                         label={key.replace(/user_/g, '').replace(/_/g, ' ').toUpperCase()}
                         name={key}
                         value={editedProfile ? editedProfile[key] ?? '' : ''}
-                        onChange={handleChange}
+                        onChange={handleTextFieldChange}
                         margin="normal"
                         fullWidth
                         variant="outlined"
---
+++ my-app/src/types/val.d.ts
@@ -0,0 +1,3 @@
+declare module 'val' {
+  export default function val(value: string): boolean;
+} 
